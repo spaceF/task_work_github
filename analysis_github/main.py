d8730cb636b7de0fb1692aa79e2da7ae5750f061
spaceF <sunday8361@gmail.com>
2020-09-16 11:08:28 +0300

    эксперементы с сортировкой словаря

--- analysis_github/main.py
+++ analysis_github/main.py
@@ -1,13 +1,16 @@
+import collections
+
 import requests
 import random
 from time import sleep
+from collections import Counter
 
 
 # ПАРАМЕТРЫ ВВОДИТЬ С ПАНЕЛИ
 URL = 'https://api.github.com'
 URL_REP = f'https://api.github.com/repos/IgnorantGuru/spacefm'
 URL_COMMITS = f'{URL_REP}/commits'
-NAMEPASS = ('sunday8361@gmail.com', 'cfb71627b6cfae6e5daf0c718b86f59c')
+NAME_PASS = ('sunday8361@gmail.com', 'cfb71627b6cfae6e5daf0c718b86f59c')
 BRANCH = 'alpha'
 DATA_START = '2018-03-28T14:33:18Z' # если не задано, то '2000-00-00T00:00:00Z'
 DATA_FINISH = '' # если не задано, то ''
@@ -15,7 +18,8 @@ DATA_FINISH = '' # если не задано, то ''
 # '2016-01-28T14:33:18Z'
 
 
-def test(**kwargs):
+def respons(**kwargs):
+    """Запрос к GitHub"""
 
     try:
         r = requests.get(url=kwargs['url'],
@@ -38,48 +42,71 @@ def test(**kwargs):
         return r.json()
 
 
+def pretty_table(rows, column_count, column_spacing=4):
+    """Построение таблицы"""
+
+    aligned_columns = []
+    for column in range(column_count):
+        column_data = list(map(lambda row: row[column], rows))
+        aligned_columns.append((max(map(len, column_data)) + column_spacing, column_data))
+
+    for row in range(len(rows)):
+        aligned_row = map(lambda x: (x[0], x[1][row]), aligned_columns)
+        yield ''.join(map(lambda x: x[1] + ' ' * (x[0] - len(x[1])), aligned_row))
+
+
 if __name__ == '__main__':
     print(f"Sign in GitHub")
     sleep(random.randint(1, 4))
-    signin = test(url=URL, login=NAMEPASS, timeout=3, params='')
-    # print(signin)
+    sign_in = respons(url=URL, login=NAME_PASS, timeout=3, params='')
+    # print(sign_in)
 
-    # Получаем репозиторий для анализа
-    print(f"Get repos")
-    sleep(random.randint(1, 4))
-    all_rep = test(url=URL_REP, login='', timeout=3,
-                   params={"since": DATA_START,
-                           "until": DATA_FINISH})
-    # print(f"{all_rep}")
-    print(f"Name repository: {all_rep['name']}\n"
-          f"Default branch: {all_rep['default_branch']}\n")
-
-    # Получаем коммиты репозитория
     print(f"Get commits")
     sleep(random.randint(1, 4))
-    all_commits = test(url=URL_COMMITS, login='', timeout=3,
-                       params={"sha": BRANCH,
+    all_commits = respons(url=URL_COMMITS, login='', timeout=3,
+                          params={"sha": BRANCH,
                                "since": DATA_START,
                                "until": DATA_FINISH}
-                       )
+                          )
     # print(all_commits)
 
+    # Парсим авторов коммитов
+    author_name = []
+    for i in range(0, 29, 1):
+        try:
+            author_name.append(
+                all_commits[i]['commit']['author']['name']
+            )
+        except IndexError:
+            print(f'All names of the commit authors collected!\n\n')
+            break
+    # Считаем коммиты авторов
+    a = Counter(author_name).most_common(30)
+    print(a)
 
-    for i in range(0, 29 ,1):
-        # commit = all_commits[0]['commit']
-        author_name = all_commits[0]['commit']['author']['name']
+    # Разделяем словарь на два списка
+    # l1 = []
+    # l2 = []
+    # for key, val in a.items():
+    #     l1.append(key)
+    #     l2.append(val)
+    # print(f'{l1}\n{l2}')
 
-    print(commit)
-    print(author_name)
+    # Подготовка данных для построения таблицы
+    # author_name = [['Name Author', 'Number commit']]
+    # for i in range(0, len(l1), 1):
+    #     try:
+    #         author_name.append((str(l1[i]), str(l2[i])))
+    #     except IndexError:
+    #         print(f'Index Error\n')
+    #         break
+    # # print(author_name)
+#
+    # # Построение таблицы
+    # print(f'Most active author')
+    # for line in pretty_table(author_name, 2):
+    #     print(line)
 
-    #print(author_)
 
-    #for i in range(0, 29, 1):
-      #  try:
-        #    c = all_commits[i]['commit']
-       # except IndexError as erri:
-        #    print(f'Index Error: {erri}')
-        #    break
-       # else:
-         #   print(c)
+
 
